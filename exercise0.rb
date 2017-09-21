6ae38cda24c2ba61e3f3bd59b39a766e685d59bb
Marian Serna <marianhalliday@gmail.com>
2017-09-20 23:18:38 -0400

    exercise 0

---
+++ exercise0.rb
@@ -0,0 +1,30 @@
+fav_colours = ['orange', 'magenta']
+friends_ages = [31, 32, 22]
+heads = [true, true, false, true, false]
+fav_artists = ['Adele', 'Bomba', 'Soda']
+fav_colors_sym = [:orange, :magenta]
+
+meanings = {
+  :alegre => "being happy",
+  :cansado => "not wanting to do anything",
+  :emocionado => "someone is excited"
+}
+
+fav_movies = {
+  :movie1 => 1992,
+  :movie2 => 2005,
+  :movie3 => 2008
+}
+
+cities = {
+  :Amsterdam => 813562,
+  :Bern => 406900,
+  :Medellin => 2500000
+}
+
+closest_people = {
+  :Leigh => 32,
+  :Mauricio => 22,
+  :Alba => 52,
+  :Jorge => 53
+}
